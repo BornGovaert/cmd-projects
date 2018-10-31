89c4708d4c90e22f7ebf46f9941fff98d0620b93
7515767 <born.govaert2@cm.be>
2018-10-31 12:41:44 +0100

    cmd-projects

---
+++ commandline.bat
@@ -0,0 +1,8 @@
+echo off
+set arg1=%*
+shift
+
+start "" "c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.google.be/search?q=%arg1%&rlz=1C1GGRV_enNL782NL782&oq=open+browser+with+command+line&aqs=chrome..69i57j0l5.9538j0j9&sourceid=chrome&ie=UTF-8" 
+
+
+
---
+++ sqldeveloper.bat
@@ -0,0 +1,1 @@
+C:\Users\7515767\Desktop\Programs\sqldeveloper-17.4.1.054.0712-x64\sqldeveloper\sqldeveloper.exe
